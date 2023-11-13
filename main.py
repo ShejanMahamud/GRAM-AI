f6151126df880ae842e2b196e88fe9ca641d7c6f
ShejanMahamud <mdshejanmahamud006@gmail.com>
2023-11-14 00:42:26 +0600

    fixed

--- main.py
+++ main.py
@@ -23,20 +23,10 @@ dp = Dispatcher(bot)
 async def send(message: types.Message):
     try:
         chatbot = await AsyncChatbot.create(secure_1PSID, secure_1PSIDTS)
-        await message.answer('Gram AI typing...')
+        await message.answer('Google Bard writes...')
         await message.answer_chat_action('typing')
         answer = await chatbot.ask(message.text)
-
-        inline_keyboard = types.InlineKeyboardMarkup(
-            inline_keyboard=[
-                [
-                    types.InlineKeyboardButton(text='Button 1', url='https://example.com/button1'),
-                    types.InlineKeyboardButton(text='Button 2', url='https://example.com/button2'),
-                ],
-            ]
-        )
-
-        await message.answer(answer.get('content'), parse_mode="markdown", reply_markup=inline_keyboard)
+        await message.answer(answer.get('content'), parse_mode="markdown")
     except Exception as ex:
         await message.answer(str(ex))
 
